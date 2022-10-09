18a2f8f429af851b8c8a781260f139f4da12bc7a
Arnel Imperial <arneli74@protonmail.com>
2022-10-09 19:06:51 +0300

    🐛 fix(index.js): solution for exercise 3.1

--- index.js
+++ index.js
@@ -6,10 +6,34 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello, World!</h1>");
 });
 
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
 
 const Port = 3001;
-
 app.listen(Port, () => {
   console.log(`Server running on port ${Port}`);
 });
