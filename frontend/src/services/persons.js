20b1500e3499637f5adad36176fa85673665d12a
Arnel Imperial <arneli74@protonmail.com>
2022-10-11 20:58:54 +0300

    🐛 fix(frontend): add environment variables

--- frontend/src/services/persons.js
+++ frontend/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:3001";
+const baseURL = process.env.REACT_APP_BASE_URL;
 
 const getAll = () => {
   return axios.get(`${baseURL}/api/persons`);
