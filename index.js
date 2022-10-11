4d74f652e9a432135e472375ab7e25c2a7760ebd
Arnel Imperial <arneli74@protonmail.com>
2022-10-11 21:11:58 +0300

    🐛 fix(index): edit files

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello, World!</h1>");
-});
-
-
-
 const Port = 3001;
 
 app.listen(Port, () => {
